1418f05a2aff512aae06c4984843ba900c7d6476
Khai Pong Teoh <khaipongteoh@gmail.com>
2024-05-29 23:31:45 +0100

    remove cache

---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,10 @@
+import { SignedIn, SignedOut, SignUpButton, SignInButton, UserButton } from "@clerk/clerk-react"
+
+
+const Auth = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Auth
